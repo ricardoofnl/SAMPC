c81cfa4f61081cdc4e002480f1c89547d7f876c9
ricardoofnl <raditiyaeltamricardo@outlook.de>
2026-08-17 23:30:59 +0700

    re(exgui_lazarus): query servers and fill the list, player and rules views

--- exgui_lazarus/main.pas
+++ exgui_lazarus/main.pas
@@ -6,12 +6,22 @@ interface
 
 uses
   Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ComCtrls, Menus,
-  ExtCtrls, StdCtrls, ValEdit, Grids, TAGraph, Types
-  {$IFDEF WINDOWS}, Windows, Registry{$ENDIF};
+  ExtCtrls, StdCtrls, ValEdit, Grids, TAGraph, TASeries, Types, SampQuery
+  {$IFDEF WINDOWS}, Windows, WinSock2, Registry{$ENDIF};
 
 type
 
-  // subset of the Delphi TServerInfo, only the fields lvServers shows
+  TQueryPlayer = record
+    Name: string;
+    Score: Integer;
+  end;
+
+  TQueryRule = record
+    Name: string;
+    Value: string;
+  end;
+
+  // subset of the Delphi TServerInfo, only what the browser shows
   TServerEntry = record
     Address: string;
     Port: Word;
@@ -20,8 +30,12 @@ type
     MaxPlayers: Integer;
     Ping: Integer;
     Mode: string;
-    Language: string;
+    Map: string;
     Passworded: Boolean;
+    ServerPassword: string;
+    RconPassword: string;
+    aPlayers: array of TQueryPlayer;
+    aRules: array of TQueryRule;
   end;
 
   { TfmMain }
@@ -128,12 +142,26 @@ type
     procedure ToggleStatusBar(Sender: TObject);
     procedure tsServerListsChange(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure FormDestroy(Sender: TObject);
     procedure lvServersSelectItem(Sender: TObject; Item: TListItem;
       Selected: Boolean);
   private
-    // favourites, held in memory only, nothing queries or persists them yet
+    // favourites, loaded from and written back to USERDATA.DAT
     Servers: array of TServerEntry;
+    QuerySocket: TSocket;
+    QueryQueue: TStringList;
+    tmrQuery: TTimer;
     function FindServer(const AAddress: string; APort: Word): Integer;
+    procedure QueueServer(AIndex: Integer);
+    procedure SendQueries(AIndex: Integer; APing, AInfo, AClients, ARules: Boolean);
+    procedure ParseQueryReply(const ABuf: TQueryBuffer; ALen: Integer);
+    procedure PollQueries(Sender: TObject);
+    procedure UpdateDetailViews;
+    function ReadPlayerName: string;
+    procedure WritePlayerName(const AName: string);
+    function UserDataPath: string;
+    procedure LoadFavorites(const AFileName: string; AAppend: Boolean);
+    procedure SaveFavorites(const AFileName: string);
     procedure UpdateServerList;
     procedure UpdateSelectionUI;
     function SelectedServer: Integer;
@@ -240,8 +268,13 @@ begin
     edSIMode.Text := Servers[iSel].Mode
   else
     edSIMode.Text := '- - -';
-  edSIMap.Text := '- - -';
+  if Servers[iSel].Map <> '' then
+    edSIMap.Text := Servers[iSel].Map
+  else
+    edSIMap.Text := '- - -';
   gbInfo.Caption := ' ' + Servers[iSel].HostName + ' ';
+
+  UpdateDetailViews;
 end;
 
 procedure TfmMain.lvServersSelectItem(Sender: TObject; Item: TListItem;
@@ -252,7 +285,407 @@ end;
 
 procedure TfmMain.FormCreate(Sender: TObject);
 begin
+  QuerySocket := INVALID_SOCKET;
+  QueryQueue := TStringList.Create;
+
+  QueryOpen(QuerySocket);
+
+  // the delphi version drives this from WSAAsyncSelect, a timer keeps the
+  // socket handling out of the window procedure
+  tmrQuery := TTimer.Create(Self);
+  tmrQuery.Interval := 100;
+  tmrQuery.OnTimer := @PollQueries;
+  tmrQuery.Enabled := True;
+
+  lebName.Text := ReadPlayerName;
+
+  LoadFavorites(UserDataPath, False);
+  UpdateServerList;
   UpdateSelectionUI;
+end;
+
+procedure TfmMain.FormDestroy(Sender: TObject);
+begin
+  WritePlayerName(lebName.Text);
+  SaveFavorites(UserDataPath);
+
+  if tmrQuery <> nil then
+    tmrQuery.Enabled := False;
+
+  QueryClose(QuerySocket);
+  FreeAndNil(QueryQueue);
+end;
+
+//----------------------------------------------------------------------------
+// query
+
+procedure TfmMain.QueueServer(AIndex: Integer);
+begin
+  if (AIndex < 0) or (AIndex > High(Servers)) then Exit;
+  if QueryQueue = nil then Exit;
+
+  if QueryQueue.IndexOf(IntToStr(AIndex)) < 0 then
+    QueryQueue.Add(IntToStr(AIndex));
+end;
+
+procedure TfmMain.SendQueries(AIndex: Integer; APing, AInfo, AClients, ARules: Boolean);
+var
+  dwAddr: LongWord;
+begin
+  if (AIndex < 0) or (AIndex > High(Servers)) then Exit;
+  if QuerySocket = INVALID_SOCKET then Exit;
+
+  dwAddr := QueryResolve(Servers[AIndex].Address);
+  if dwAddr = 0 then Exit;
+
+  if AInfo then
+    QuerySend(QuerySocket, dwAddr, Servers[AIndex].Port, QUERY_OP_INFO, 0);
+  if AClients then
+    QuerySend(QuerySocket, dwAddr, Servers[AIndex].Port, QUERY_OP_CLIENTS, 0);
+  if ARules then
+    QuerySend(QuerySocket, dwAddr, Servers[AIndex].Port, QUERY_OP_RULES, 0);
+  if APing then
+    QuerySend(QuerySocket, dwAddr, Servers[AIndex].Port, QUERY_OP_PING, QueryTick);
+end;
+
+// mirrors TfmMain.QueryServerInfoParse, every string is length prefixed and is
+// not terminated
+procedure TfmMain.ParseQueryReply(const ABuf: TQueryBuffer; ALen: Integer);
+var
+  sIP: string;
+  wPort: Word;
+  iIdx, iPos, i, iCount: Integer;
+  dwTick: LongWord;
+
+  function ReadStr(ALenBytes: Integer): string;
+  var
+    n: LongWord;
+  begin
+    Result := '';
+    n := 0;
+    if ALenBytes = 4 then
+    begin
+      if iPos + 4 > ALen then Exit;
+      Move(ABuf[iPos], n, 4);
+      Inc(iPos, 4);
+    end
+    else
+    begin
+      if iPos + 1 > ALen then Exit;
+      n := ABuf[iPos];
+      Inc(iPos, 1);
+    end;
+    if (n = 0) or (iPos + Integer(n) > ALen) then Exit;
+    SetLength(Result, n);
+    Move(ABuf[iPos], Result[1], n);
+    Inc(iPos, Integer(n));
+  end;
+
+begin
+  // 4 magic, 4 ip, 2 port, 1 opcode
+  if ALen < QUERY_HEADER_LEN then Exit;
+  if (ABuf[0] <> Ord('S')) or (ABuf[1] <> Ord('A')) or
+     (ABuf[2] <> Ord('M')) or (ABuf[3] <> Ord('P')) then Exit;
+
+  sIP := Format('%d.%d.%d.%d', [ABuf[4], ABuf[5], ABuf[6], ABuf[7]]);
+  Move(ABuf[8], wPort, 2);
+
+  iIdx := FindServer(sIP, wPort);
+  if iIdx < 0 then Exit;
+
+  iPos := QUERY_HEADER_LEN;
+
+  case Chr(ABuf[10]) of
+    QUERY_OP_INFO:
+      begin
+        if iPos + 5 > ALen then Exit;
+        Servers[iIdx].Passworded := ABuf[iPos] <> 0;
+        Inc(iPos, 1);
+        Move(ABuf[iPos], wPort, 2);
+        Servers[iIdx].Players := wPort;
+        Inc(iPos, 2);
+        Move(ABuf[iPos], wPort, 2);
+        Servers[iIdx].MaxPlayers := wPort;
+        Inc(iPos, 2);
+
+        Servers[iIdx].HostName := ReadStr(4);
+        Servers[iIdx].Mode := ReadStr(4);
+        Servers[iIdx].Map := ReadStr(4);
+      end;
+
+    QUERY_OP_CLIENTS:
+      begin
+        if iPos + 2 > ALen then Exit;
+        Move(ABuf[iPos], wPort, 2);
+        Inc(iPos, 2);
+        iCount := wPort;
+
+        SetLength(Servers[iIdx].aPlayers, iCount);
+        for i := 0 to iCount - 1 do
+        begin
+          Servers[iIdx].aPlayers[i].Name := ReadStr(1);
+          if iPos + 4 > ALen then
+          begin
+            SetLength(Servers[iIdx].aPlayers, i);
+            Break;
+          end;
+          Move(ABuf[iPos], Servers[iIdx].aPlayers[i].Score, 4);
+          Inc(iPos, 4);
+        end;
+      end;
+
+    QUERY_OP_RULES:
+      begin
+        if iPos + 2 > ALen then Exit;
+        Move(ABuf[iPos], wPort, 2);
+        Inc(iPos, 2);
+        iCount := wPort;
+
+        SetLength(Servers[iIdx].aRules, iCount);
+        for i := 0 to iCount - 1 do
+        begin
+          Servers[iIdx].aRules[i].Name := ReadStr(1);
+          Servers[iIdx].aRules[i].Value := ReadStr(1);
+        end;
+      end;
+
+    QUERY_OP_PING:
+      begin
+        if ALen < 15 then Exit;
+        Move(ABuf[11], dwTick, 4);
+        Servers[iIdx].Ping := Integer(QueryTick - dwTick);
+        if Servers[iIdx].Ping < 1 then
+          Servers[iIdx].Ping := 1;
+      end;
+  end;
+
+  UpdateServerList;
+  if SelectedServer = iIdx then
+    UpdateSelectionUI;
+end;
+
+procedure TfmMain.PollQueries(Sender: TObject);
+var
+  Buf: TQueryBuffer;
+  iLen, iIdx, iGuard: Integer;
+begin
+  // one queued server per tick keeps a long favourites list from flooding
+  if (QueryQueue <> nil) and (QueryQueue.Count > 0) then
+  begin
+    iIdx := StrToIntDef(QueryQueue[0], -1);
+    QueryQueue.Delete(0);
+    SendQueries(iIdx, True, True, True, True);
+  end;
+
+  // drain whatever came back since the last tick
+  iGuard := 0;
+  while QueryReceive(QuerySocket, Buf, iLen) and (iGuard < 64) do
+  begin
+    ParseQueryReply(Buf, iLen);
+    Inc(iGuard);
+  end;
+end;
+
+procedure TfmMain.UpdateDetailViews;
+var
+  iSel, i: Integer;
+  Item: TListItem;
+begin
+  iSel := SelectedServer;
+
+  lvPlayers.BeginUpdate;
+  lvRules.BeginUpdate;
+  try
+    lvPlayers.Items.Clear;
+    lvRules.Items.Clear;
+    if iSel < 0 then Exit;
+
+    for i := 0 to High(Servers[iSel].aPlayers) do
+    begin
+      Item := lvPlayers.Items.Add;
+      Item.Caption := Servers[iSel].aPlayers[i].Name;
+      Item.SubItems.Add(IntToStr(Servers[iSel].aPlayers[i].Score));
+    end;
+
+    for i := 0 to High(Servers[iSel].aRules) do
+    begin
+      Item := lvRules.Items.Add;
+      Item.Caption := Servers[iSel].aRules[i].Name;
+      Item.SubItems.Add(Servers[iSel].aRules[i].Value);
+    end;
+  finally
+    lvRules.EndUpdate;
+    lvPlayers.EndUpdate;
+  end;
+end;
+
+//----------------------------------------------------------------------------
+// persistence
+
+function TfmMain.ReadPlayerName: string;
+{$IFDEF WINDOWS}
+var
+  Reg: TRegistry;
+{$ENDIF}
+begin
+  Result := '';
+{$IFDEF WINDOWS}
+  Reg := TRegistry.Create;
+  try
+    Reg.RootKey := HKEY_CURRENT_USER;
+    if Reg.OpenKeyReadOnly('Software\SAMP') then
+    begin
+      if Reg.ValueExists('PlayerName') then
+        Result := Reg.ReadString('PlayerName');
+      Reg.CloseKey;
+    end;
+  finally
+    Reg.Free;
+  end;
+{$ENDIF}
+end;
+
+procedure TfmMain.WritePlayerName(const AName: string);
+{$IFDEF WINDOWS}
+var
+  Reg: TRegistry;
+{$ENDIF}
+begin
+{$IFDEF WINDOWS}
+  Reg := TRegistry.Create;
+  try
+    Reg.RootKey := HKEY_CURRENT_USER;
+    if Reg.OpenKey('Software\SAMP', True) then
+    begin
+      Reg.WriteString('PlayerName', AName);
+      Reg.CloseKey;
+    end;
+  finally
+    Reg.Free;
+  end;
+{$ENDIF}
+end;
+
+function TfmMain.UserDataPath: string;
+begin
+  Result := ExtractFilePath(ParamStr(0)) + 'USERDATA.DAT';
+end;
+
+// the binary layout ImportFavorites and ExportFavorites use in the Delphi
+// original: tag SAMP, int32 version, int32 count, then per server the address,
+// port, hostname, server password and rcon password
+procedure TfmMain.LoadFavorites(const AFileName: string; AAppend: Boolean);
+var
+  Stream: TFileStream;
+  FileTag: array[0..3] of AnsiChar;
+  iVersion, iCount, iLen, i, iBase: Integer;
+
+  function ReadStr: string;
+  var
+    n: Integer;
+  begin
+    Result := '';
+    n := 0;
+    if Stream.Read(n, 4) <> 4 then Exit;
+    if (n <= 0) or (n > 1024) then Exit;
+    SetLength(Result, n);
+    Stream.ReadBuffer(Result[1], n);
+  end;
+
+begin
+  if not FileExists(AFileName) then Exit;
+
+  Stream := TFileStream.Create(AFileName, fmOpenRead or fmShareDenyNone);
+  try
+    if Stream.Read(FileTag[0], 4) <> 4 then Exit;
+    if (FileTag[0] <> 'S') or (FileTag[1] <> 'A') or (FileTag[2] <> 'M') or (FileTag[3] <> 'P') then
+    begin
+      MessageDlg('Invalid SA:MP file.', mtError, [mbOK], 0);
+      Exit;
+    end;
+
+    iVersion := 0;
+    if Stream.Read(iVersion, 4) <> 4 then Exit;
+    if iVersion <> 1 then
+    begin
+      MessageDlg('Bad SA:MP favorites file version.', mtError, [mbOK], 0);
+      Exit;
+    end;
+
+    iCount := 0;
+    if Stream.Read(iCount, 4) <> 4 then Exit;
+
+    if not AAppend then
+      SetLength(Servers, 0);
+
+    for i := 1 to iCount do
+    begin
+      if Stream.Position >= Stream.Size then Break;
+
+      iBase := Length(Servers);
+      SetLength(Servers, iBase + 1);
+
+      Servers[iBase].Address := ReadStr;
+      iLen := 0;
+      Stream.Read(iLen, 4);
+      Servers[iBase].Port := Word(iLen);
+      Servers[iBase].HostName := ReadStr;
+      Servers[iBase].ServerPassword := ReadStr;
+      Servers[iBase].RconPassword := ReadStr;
+
+      Servers[iBase].Ping := 0;
+      Servers[iBase].Players := 0;
+      Servers[iBase].MaxPlayers := 0;
+
+      // a duplicate address collapses back onto the existing row
+      if (Servers[iBase].Address = '') or
+         (FindServer(Servers[iBase].Address, Servers[iBase].Port) <> iBase) then
+        SetLength(Servers, iBase)
+      else
+        QueueServer(iBase);
+    end;
+  finally
+    Stream.Free;
+  end;
+end;
+
+procedure TfmMain.SaveFavorites(const AFileName: string);
+var
+  Stream: TFileStream;
+  FileTag: array[0..3] of AnsiChar = ('S', 'A', 'M', 'P');
+  iValue, i: Integer;
+
+  procedure WriteStr(const AText: string);
+  var
+    n: Integer;
+  begin
+    n := Length(AText);
+    Stream.WriteBuffer(n, 4);
+    if n > 0 then
+      Stream.WriteBuffer(AText[1], n);
+  end;
+
+begin
+  Stream := TFileStream.Create(AFileName, fmCreate);
+  try
+    Stream.WriteBuffer(FileTag[0], 4);
+    iValue := 1;
+    Stream.WriteBuffer(iValue, 4);
+    iValue := Length(Servers);
+    Stream.WriteBuffer(iValue, 4);
+
+    for i := 0 to High(Servers) do
+    begin
+      WriteStr(Servers[i].Address);
+      iValue := Servers[i].Port;
+      Stream.WriteBuffer(iValue, 4);
+      WriteStr(Servers[i].HostName);
+      WriteStr(Servers[i].ServerPassword);
+      WriteStr(Servers[i].RconPassword);
+    end;
+  finally
+    Stream.Free;
+  end;
 end;
 
 procedure TfmMain.UpdateServerList;
@@ -275,7 +708,7 @@ begin
       else
         Item.SubItems.Add('');
       Item.SubItems.Add(Servers[i].Mode);
-      Item.SubItems.Add(Servers[i].Language);
+      Item.SubItems.Add(Servers[i].Map);
     end;
   finally
     lvServers.EndUpdate;
@@ -314,13 +747,35 @@ begin
 end;
 
 procedure TfmMain.QuickConnectClick(Sender: TObject);
+var
+  sInput, sHost: string;
+  wPort: Word;
 begin
+  sInput := '';
+  if not InputQuery('Quick Connect', 'Enter server HOST:PORT...', sInput) then
+    Exit;
 
+  SplitHostPort(sInput, sHost, wPort);
+  if sHost = '' then
+    Exit;
+
+  ServerConnect(sHost, wPort, '');
 end;
 
 procedure TfmMain.RefreshServerClick(Sender: TObject);
+var
+  iSel: Integer;
 begin
+  iSel := SelectedServer;
+  if iSel < 0 then
+  begin
+    // nothing picked, refresh the whole list the way the toolbar implies
+    for iSel := 0 to High(Servers) do
+      QueueServer(iSel);
+    Exit;
+  end;
 
+  SendQueries(iSel, True, True, True, True);
 end;
 
 procedure TfmMain.ServerPropertiesClick(Sender: TObject);
@@ -632,6 +1087,12 @@ begin
     Servers[i] := Servers[i + 1];
   SetLength(Servers, Length(Servers) - 1);
 
+  // the queue holds indexes, which have all just shifted
+  if QueryQueue <> nil then
+    QueryQueue.Clear;
+
+  SaveFavorites(UserDataPath);
+
   UpdateServerList;
   UpdateSelectionUI;
 end;
@@ -665,15 +1126,20 @@ begin
   begin
     Address := sHost;
     Port := wPort;
-    // nothing queries the server yet, so show what was typed
-    HostName := Format('(No info) %s:%d', [sHost, wPort]);
+    // replaced by the hostname as soon as the info reply lands
+    HostName := Format('(Retrieving info...) %s:%d', [sHost, wPort]);
     Players := 0;
     MaxPlayers := 0;
     Ping := 0;
     Mode := '';
-    Language := '';
+    Map := '';
     Passworded := False;
+    ServerPassword := '';
+    RconPassword := '';
   end;
+
+  QueueServer(High(Servers));
+  SaveFavorites(UserDataPath);
 
   UpdateServerList;
   lvServers.ItemIndex := High(Servers);
